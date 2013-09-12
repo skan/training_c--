086c55428887f5001ac9d80fb9e00624a5644a60
Skander <Skander.hamza@gmail.com>
2013-09-12 23:00:13 +0200

    add rectangle

--- first_tp/first_tp.cpp
+++ first_tp/first_tp.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+int carre (int nombre)
+{
+   int resultat (0);
+   resultat = nombre * nombre;
+   return resultat;
+}
+
+int dessine_rectangle (int height, int width)
+{
+   int i(0), j(0);
+   for (i = 0; i < height; i++)
+   {
+      for (j = 0; j < width; j++)
+      {
+         cout << "*";
+      }
+      cout << endl ;
+   }
+   return 0;
+}
 
 int main()
 {
@@ -16,5 +38,12 @@ int main()
    reste = a % b;
    c = a / b;
    cout << a << " / " << b << " = " << c << " et il reste : " << reste << endl;
+   c = sqrt (a);
+   cout << "la racine de " << a << " = " << c << endl;
+   c = pow(a,b);
+   cout << a << " ^ " << b << " = " << c << endl;
+   c = carre (a);
+   cout << "le carre de " << a << " = " << c << endl;
+   dessine_rectangle(a,b);
    return 0;
 }
