696c698f5f118fb9aa34ba5b31a21e20843f31dd
Skander <Skander.hamza@gmail.com>
2013-09-29 17:02:15 +0200

    chap 8-polymorphisme: collection heterogene

--- chap8-polymorphisme/main.cpp
+++ chap8-polymorphisme/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Vehicule.h"
 #include "Moto.h"
 #include "Voiture.h"
@@ -14,10 +15,20 @@ void presenter(Vehicule const& v) ///Presente le vehicule passé en argument
 int main()
 {
 
-   Vehicule *v1(0);
-   v1 = new Voiture(45,2);
-   v1->affiche();
-   delete v1;
+   vector<Vehicule*> listeVehicules;
+   listeVehicules.push_back(new Voiture(15000, 5));
+   listeVehicules.push_back(new Voiture(12000, 3));
+   listeVehicules.push_back(new Moto(2000, 212.5));
+
+   listeVehicules[0]->affiche();
+   listeVehicules[1]->affiche();
+   listeVehicules[2]->affiche();
+
+   for (int i(0); i < listeVehicules.size(); ++i)
+   {
+      delete listeVehicules[i];
+      listeVehicules[i] = 0;
+   }
 
    return 0;
 }
