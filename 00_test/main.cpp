814e62c091d5a373cea7221a3e15189513a5a5be
Skander <Skander.hamza@gmail.com>
2013-09-30 19:34:14 +0200

    qt. qfont

--- 00_test/main.cpp
+++ 00_test/main.cpp
@@ -3,9 +3,12 @@
 
 int main (int argc, char *argv[])
 {
-    QApplication app(argc,argv);
+    QApplication    app(argc,argv);
+    QPushButton     bouton ("Salut les Zeros, la forme ?");
 
-    QPushButton bouton ("Salut les Zeros, la forme ?");
+    bouton.setText("pimp mon bouton");
+    bouton.setToolTip("texte d'aide");
+    bouton.setFont(QFont("Courier"));
     bouton.show();
     return app.exec();
 }
