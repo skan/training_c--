34ceae4dd658e2a59bd5ebd407144a4d4bc0ca2a
Skander <Skander.hamza@gmail.com>
2013-09-30 21:35:34 +0200

    qt. slot and signals - quit

--- 00_test/Mafenetre.cpp
+++ 00_test/Mafenetre.cpp
@@ -1,16 +1,5 @@
 #include "MaFenetre.h"
 
-MaFenetre::MaFenetre() : QWidget()
-{
-    setFixedSize(300,150);
-
-    m_bouton = new QPushButton("Pimp mon bouton !", this);
-    m_bouton->setFont(QFont("Comic Sans MS",14));
-    m_bouton->setCursor(Qt::PointingHandCursor);
-    m_bouton->setIcon(QIcon("smile.png"));
-    m_bouton->move(60,50);
-}
-
 MaFenetre::MaFenetre(int w, int h) : QWidget()
 {
     setFixedSize(w,h);
@@ -20,5 +9,6 @@ MaFenetre::MaFenetre(int w, int h) : QWidget()
     m_bouton->setCursor(Qt::PointingHandCursor);
     m_bouton->setIcon(QIcon("smile.png"));
     m_bouton->move(60,50);
+    QObject::connect(m_bouton, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
 
