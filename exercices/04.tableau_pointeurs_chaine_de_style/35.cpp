a50d9d5ad3868544884122956e6bf6078c977a18
Skander <Skander.hamza@gmail.com>
2013-10-03 23:28:20 +0200

    exercice 35, tuning

--- exercices/04.tableau_pointeurs_chaine_de_style/35.cpp
+++ exercices/04.tableau_pointeurs_chaine_de_style/35.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 using namespace std ;
 
-void calulMaxMin (int *t, int *maximal, int *minimal)
+void calulMaxMin (int *t, int nval, int *maximal, int *minimal)
 {
    int *adt;
-   *maximal=*minimal=*(t+4);
-   for (adt = t+4 ; adt >= t ; adt--)
+   *maximal=*minimal=*(t+nval-1);
+   for (adt = t+nval-1 ; adt >= t ; adt--)
    {
       if (*(adt) > *maximal)
          *maximal = *adt;
@@ -17,15 +17,13 @@ void calulMaxMin (int *t, int *maximal, int *minimal)
 
 int main()
 {
-   int t [5] ;
+   int const nval = 5;
+   int t [nval] ;
    int i(0);
    int min(0), max(0) ;
 
-   for (i = 0; i < 5 ; i++)
-   {
-      cin >> *(t+i);
-   }
-   calulMaxMin(t,&max,&min);
+   for (i = 0; i < nval ; i++) cin >> *(t+i);
+   calulMaxMin(t,nval,&max,&min);
    cout << "la valeur max du tableau est " << max << " et la valeur min " << min << endl;
 
 #if 0
