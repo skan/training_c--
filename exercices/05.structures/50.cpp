39253fb8d7c5fa058b8abbfa093164b817fa41f8
Skander <Skander.hamza@gmail.com>
2013-10-05 20:49:47 +0200

    exercie 50: use pointer beside array

--- exercices/05.structures/50.cpp
+++ exercices/05.structures/50.cpp
@@ -40,7 +40,7 @@ void afficher (point *a)
 {
    int i(0);
    for (i= 0; i < NB; i++)
-      cout << "Point " << a[i].c << " de coordonnees " << a[i].x << " " << a[i].y << endl;
+      cout << "Point " << (a+i)->c << " de coordonnees " << a[i].x << " " << a[i].y << endl;
    return;
 }
 
