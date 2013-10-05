33e90d01ec54dd31f1abbfe55170d8d68490d9f9
Skander <Skander.hamza@gmail.com>
2013-10-05 18:49:39 +0200

    exercice 48- raz & affich

---
+++ exercices/05.structures/48.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+const int MOIS=12;
+struct enreg
+{
+   int stock;
+   float prix;
+   int ventes[MOIS];
+};
+
+void raz (enreg &);
+void afficher (enreg const &a);
+
+int main ()
+{
+   enreg lait;   
+   raz (lait);
+   afficher (lait);
+   lait.stock =1; 
+   afficher (lait);
+   raz (lait);
+   afficher (lait);
+   
+   return 0;
+}
+
+void raz (enreg &a)
+{
+   int i(0);
+   a.stock = 0;
+   a.prix = 0;
+   for (i = 0; i < MOIS ; i++) a.ventes[i]=0;
+}
+void afficher (enreg const &a)
+{
+   int i(0);
+   cout << a.stock << " " << a.prix << " " << endl;
+   for (i = 0; i < MOIS ; i++) cout << a.ventes[i] << " ";
+   cout << endl;
+}
