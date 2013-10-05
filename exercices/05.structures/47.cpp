2cebde14b0d431b71d35b81ac0f2dbbe96636d9b
Skander <Skander.hamza@gmail.com>
2013-10-05 17:38:19 +0200

    exercice 47- init struct att declaration

--- exercices/05.structures/47.cpp
+++ exercices/05.structures/47.cpp
@@ -7,21 +7,21 @@ struct point
    int x,y;
 };
 
-void afficher_par_valeur (point a);
-void afficher_par_reference (point &a);
-void afficher_par_addresse (point *a);
+void afficher_par_valeur (point );
+void afficher_par_reference (point &);
+void afficher_par_addresse (point *);
 
 int main ()
 {
-   point a;
+   point a = {'S',10,25};
 
-   a.c = 's';
-   a.x = 10;
-   a.y = 25;
+   //point* b = new point {'B', 2,23};
 
    afficher_par_valeur(a);
    afficher_par_reference(a);
    afficher_par_addresse(&a);
+
+   //afficher_par_addresse(&b);
 
    return 0;
 }
