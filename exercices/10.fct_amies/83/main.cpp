da2012260eed41c8680799ef6011e890d15bfa70
Skander <Skander.hamza@gmail.com>
2013-10-10 01:02:08 +0200

    exercice 83: class matrice ok

--- exercices/10.fct_amies/83/main.cpp
+++ exercices/10.fct_amies/83/main.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 #include "vect.h"
+#include "matrice.h"
 
 int main()
 {
    vect a(1,1,2);
    a.affiche();
+   
+   double tab[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+   matrice x(tab);
+   x.affiche();
 }
