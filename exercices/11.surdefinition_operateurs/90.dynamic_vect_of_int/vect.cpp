b4bb434f6e14d16e3ca39ff88416838242d160ae
Skander <Skander.hamza@gmail.com>
2013-10-16 19:37:09 +0200

    exercice 90: tuning

--- exercices/11.surdefinition_operateurs/90.dynamic_vect_of_int/vect.cpp
+++ exercices/11.surdefinition_operateurs/90.dynamic_vect_of_int/vect.cpp
@@ -1,11 +1,5 @@
 #include "vect.h"
 
-int vect::operator [] (int nb) const
-{
-   if (nb < 0 || nb > size ) nb = 0;
-   return *(v+nb);
-}
-
 float& vect::operator [] (int nb)
 {
    if (nb < 0 || nb > size ) nb = 0;
--- exercices/11.surdefinition_operateurs/90.dynamic_vect_of_int/vect.h
+++ exercices/11.surdefinition_operateurs/90.dynamic_vect_of_int/vect.h
@@ -5,8 +5,13 @@ class vect
    public :
       vect (int taille = 1)
       { 
-         v = new float [taille];
-         size = taille;
+         v = new float [size=taille];
+         for (int i = 0; i< size; i++)
+            v[i]=0;
+      }
+      ~vect()
+      {
+         delete v;
       }
       int operator [] (int) const;
       float& operator [] (int);
